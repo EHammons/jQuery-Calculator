4d933ca7491a90be9df03048179229e6d370177f
Stardust <52759497+EHammons@users.noreply.github.com>
2019-09-09 14:19:10 -0500

    numbers show up on screen

--- game.js
+++ game.js
@@ -5,20 +5,9 @@ $(document).ready(function() {
     var inputTwo = 0;
     var result = 0;
 
-    // var inputChoices = ["1","2","3","+","4","5","6","-","7","8","9","*","0","/","*","^","="];
-
-    // for (var i = 0; i < inputChoices.length; i++) {
-    //     var userInput = $("<button>");
-
-    //     userInput.addClass("userInput");
-    
-    //     userInput.attr("data-userInput", inputChoices[i]);
-
-    //     userInput.text(inputChoices[i]);
-      
-    // }
-
     $(".btn").on("click", function() {
+
+        $("#first-number").append(this.value);
 
         console.log(this.value);
 
