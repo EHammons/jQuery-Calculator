93f61e69e94a8c66cf0f40b7dfef785db6bb0afe
Stardust <52759497+EHammons@users.noreply.github.com>
2019-09-09 13:40:07 -0500

    Started js

--- game.js
+++ game.js
@@ -1,7 +1,31 @@
 $(document).ready(function() {
 
-    // Your code here...
+    var inputOne = 0;
+    var operator = 0;
+    var inputTwo = 0;
+    var result = 0;
+
+    var inputChoices = ["0","1","2","3","4","5","6","7","8","9","+","-","/","*","^","="];
+
+    for (var i = 0; i < inputChoices.length; i++) {
+        var input = $("<button>");
+
+        input.addclass("input");
     
+        input.attr("data-input", inputChoices[i]);
+
+        input.text(inputChoices[i]);
+
+        // console.log(input);
+        
+    }
+
+    $(".input").on("click", function() {
+
+        
+
+    })
+
     
-    });
+});
     
